05394f3e05bd67e5e35721ab661a4d37e5aa4e34
Esther White <esthersoftwaredev@gmail.com>
2025-02-13 15:17:15 +0200

    feature(sitewide): main layoutand routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'withangular';
+  title = 'WithAngular';
 }
